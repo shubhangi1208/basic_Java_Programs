8d671e6039100eb94a6905e3aacb264c87a3a443
shubhangi1208 <96242483+shubhangi1208@users.noreply.github.com>
2022-01-04 13:24:34 +0530

    ADDED:QUADRATIC PROGRAM

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+       //Variable Declaration
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Value Of A :");
+        a = sc.nextInt();
+
+        System.out.println("Enter the Value Of B :");
+        b = sc.nextInt();
+
+        System.out.println("Enter the Value Of C :");
+        c = sc.nextInt();
+
+        //Calculating Delta Value
+        int delta = ((b*b) - 4 * (a*c));
+        System.out.println("Delta ="+delta);
+
+        //Calulating Root1 of X
+        double root1X = ((-b + Math.sqrt(delta)) / (2*a));
+        System.out.println("Root 1 Of X ="+root1X);
+
+        //Calculating Root2 of X
+        double root2X = ((-b - Math.sqrt(delta)) / (2*a));
+        System.out.println("Root 2 Of X ="+root2X);
+    }
+}
